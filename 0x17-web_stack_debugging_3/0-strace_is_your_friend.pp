fc5013e6edd775845bbb4820339376dcae520268
Kolawole <kola.tella24@gmail.com>
2024-06-05 18:32:11 -0700

    0x17. Web stack debugging #3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Fixes and automates apache error using puppet.
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
 exec { 'fix-wordpress':
 	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
